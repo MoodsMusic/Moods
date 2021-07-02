1a68b0c1bc78f08fc98c6fb0444f473e1d5ea700
Nathan Peek <nmp14fsu@gmail.com>
2021-07-02 15:57:49 -0400

    Set up server basics and basic express npm packages

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+// const controllers = require("./controllers");
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/MoodsDB", { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(controllers);
+
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
