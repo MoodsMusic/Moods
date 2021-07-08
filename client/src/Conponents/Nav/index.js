9cb8fe227200f279518bede68154855b836d7b14
Nathan Peek <nmp14fsu@gmail.com>
2021-07-08 03:44:14 -0400

    Add links to nav and more styling

--- client/src/Conponents/Nav/index.js
+++ client/src/Conponents/Nav/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import "./style.css";
 
 const Nav = () => {
     return (
         <nav>
             <div className="nav-left">
-                <span>#Moods</span>
-                <span>Home</span>
+                <span><Link to="/">#Moods</Link></span>
             </div>
             <div className="nav-right">
-                <span>Login</span>
+                <span><Link to="#">Login</Link></span>
             </div>
         </nav>
     );
