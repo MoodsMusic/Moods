49ba85c95a0ffb172a86afa4c3df4dd97bfd6d37
Nathan Peek <nmp14fsu@gmail.com>
2021-07-08 02:29:11 -0400

    Create basic nav bar (not dynamic to login) with styling

--- client/src/Conponents/Nav/index.js
+++ client/src/Conponents/Nav/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import "./style.css";
 
 const Nav = () => {
     return (
-        <div>
-
-        </div>
+        <nav>
+            <div className="nav-left">
+                <span>#Moods</span>
+                <span>Home</span>
+            </div>
+            <div className="nav-right">
+                <span>Login</span>
+            </div>
+        </nav>
     );
 };
 
