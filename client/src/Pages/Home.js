99beba755170b27c77a7f51d4eeb1933a3e083e2
Nathan Peek <nmp14fsu@gmail.com>
2021-07-08 03:44:00 -0400

    Create starting home page and styling

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import piano from "../Images/piano-min.png";
 
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <main className="home">
+            <header>
+                <h1 className="text-center">#MOODS</h1>
+                <p className="text-center">A Social Media Music Platform</p>
+            </header>
+            <div className="hero">
+            </div>
+        </main>
     );
 };
 
