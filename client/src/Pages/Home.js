fd6d302c03898f5e27ee4af98f913dccad9fc926
Nathan Peek <nmp14fsu@gmail.com>
2021-07-08 16:48:32 -0400

    Add section for music and animations

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,6 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Section from '../Conponents/Section';
 
 const Home = () => {
+
+    useEffect(() => {
+        // Observer for text highlight animations
+        const wrapper = document.getElementById("highlight-effect");
+        const className = "in-view";
+
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        wrapper.classList.add(className);
+                        return;
+                    }
+                    wrapper.classList.remove(className);
+                });
+            },
+        );
+
+        observer.observe(wrapper);
+
+        // Observer for text sliding
+        const ps = document.querySelectorAll(".highlight");
+        const className2 = "slide";
+
+        const observer2 = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add(className2);
+                        return;
+                    }
+                    entry.target.classList.remove(className2);
+                });
+            },
+        );
+
+        ps.forEach((p) => {
+            observer2.observe(p.parentElement.parentElement);
+        });
+    }, []);
+
     return (
         <main className="home">
             <header>
@@ -14,17 +56,25 @@ const Home = () => {
                 </div>
             </div>
 
-            <section className="section1">
+            <Section sectionNum={"section1"}>
                 <h2 className="text-center">Music</h2>
-                <div>
+                <div id="highlight-effect">
                     <div className="left-col">
-
+                        <p><span className="highlight">Search for songs from our collection...</span></p>
+                        <p><span className="highlight">Listen to music...</span></p>
+                        <p><span className="highlight">Share your feelings or thoughts</span></p>
                     </div>
                     <div className="right-col">
-
+                        <p><span className="highlight">Create several playlists</span></p>
+                        <p><span className="highlight">Add songs to playlists</span></p>
+                        <p><span className="highlight">Share playlists with friends</span></p>
                     </div>
                 </div>
-            </section>
+            </Section>
+
+            <Section sectionNum={"section2"}>
+                <p className="text-center">Create posts or diary entries.</p>
+            </Section>
         </main>
     );
 };
