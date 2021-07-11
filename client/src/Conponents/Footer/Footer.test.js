d1bc134e3e982f16571fa271431313c2ac634e81
Nathan Peek <nmp14fsu@gmail.com>
2021-07-11 02:59:29 -0400

    Update test to mock useLocation

--- client/src/Conponents/Footer/Footer.test.js
+++ client/src/Conponents/Footer/Footer.test.js
@@ -1,10 +1,24 @@
 import { render } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import Footer from "./index";
 
+console.error = jest.fn();
+
 describe("Footer component", () => {
     test("It should render", () => {
-        const { getByText } = render(<Footer />);
+        jest.mock("react-router-dom", () => ({
+            ...jest.requireActual("react-router-dom"),
+            useLocation: () => ({
+                pathname: "/home"
+            })
+        }));
+
+        const { getByText } = render(
+            <Router>
+                <Footer />
+            </Router>
+        );
 
         getByText("#Moods");
         getByText("Information");
