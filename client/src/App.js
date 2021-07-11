335ef22dff9b90247377fb2a405a8c5bf0a463fc
Nathan Peek <nmp14fsu@gmail.com>
2021-07-11 04:02:25 -0400

    Add scrolling animation for nav-bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Footer from "./Conponents/Footer";
@@ -7,6 +8,27 @@ import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 
 function App() {
+
+  // Add scrolling conditions to add or remove classes to/from nav-bar
+  // Classes add or remove background from nav-bar depending if it is at top of page or not
+  useEffect(() => {
+    const nav = document.querySelector(".nav");
+
+    document.addEventListener('scroll', () => {
+      const isScrolled = window.scrollY > 60;
+      const isTop = window.scrollY === 0;
+
+      if (isScrolled) {
+        nav.classList.remove("nav-not-scrolled")
+        nav.classList.add("nav-scrolled");
+      } else if (isTop) {
+        nav.classList.remove("nav-scrolled");
+        nav.classList.add("nav-not-scrolled");
+      }
+    });
+
+  }, []);
+
   return (
     <Router>
       <Nav />
