5763fd9dbb9105cb46d0755995e5cc6613db34e2
Nathan Peek <nmp14fsu@gmail.com>
2021-07-11 02:33:45 -0400

    Change form button colors

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -29,6 +29,7 @@ const useStyles = makeStyles((theme) => ({
     },
     submit: {
         margin: theme.spacing(3, 0, 2),
+        background: "teal"
     },
 }));
 
--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -27,6 +27,7 @@ const useStyles = makeStyles((theme) => ({
     },
     submit: {
         margin: theme.spacing(3, 0, 2),
+        background: "teal"
     },
 }));
 
