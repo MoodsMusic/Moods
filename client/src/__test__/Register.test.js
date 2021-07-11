cfb342c13b8452d9ac3977030d783242576e7b14
Nathan Peek <nmp14fsu@gmail.com>
2021-07-11 03:36:03 -0400

    Add tests for register page

---
+++ client/src/__test__/Register.test.js
@@ -0,0 +1,43 @@
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import Register from "../pages/Register";
+
+describe("Register page", () => {
+    test("It renders and doesnt show nav login", () => {
+        const { getByText } = render(
+            <Register />
+        );
+
+        getByText("Sign Up");
+        getByText("First Name");
+        getByText("Last Name");
+        getByText("Username");
+        getByText("Email Address");
+        getByText("Password");
+    });
+
+    test("Can enter information", () => {
+        render(
+            <Register />
+        );
+
+        const fName = document.getElementById("firstName");
+        const lName = document.getElementById("lastName");
+        const username = document.getElementById("username");
+        const password = document.getElementById("password");
+        const email = document.getElementById("email");
+
+        userEvent.type(fName, "Jest first");
+        userEvent.type(lName, "Jest last");
+        userEvent.type(username, "Jest username");
+        userEvent.type(password, "Jest password");
+        userEvent.type(email, "Jest@mail.com");
+
+        expect(fName.value).toBe("Jest first");
+        expect(lName.value).toBe("Jest last");
+        expect(username.value).toBe("Jest username");
+        expect(password.value).toBe("Jest password");
+        expect(email.value).toBe("Jest@mail.com");
+    });
+});
