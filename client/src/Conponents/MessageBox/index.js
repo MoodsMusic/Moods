1f260cdad7d313ede589fbd53f4d636124d0eda1
Nathan Peek <nmp14fsu@gmail.com>
2021-07-13 02:33:42 -0400

    Fix color

--- client/src/Conponents/MessageBox/index.js
+++ client/src/Conponents/MessageBox/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 const MessageBox = (props) => {
     return (
         <div className={`message-box alert alert-${props.variant || 'info'} text-center`}
-            style={{ background: props.variant ? props.variant : "cyan", color: props.variant ? "rbg(255,255,255)" : "black" }}>
+            style={{ background: props.variant ? props.variant : "cyan", color: props.variant ? "white" : "black" }}>
             {props.children}
         </div>
     );
