112aa840e6b0eaa0b32b32b2d4905b13707c754b
Nathan Peek <nmp14fsu@gmail.com>
2021-07-13 02:29:26 -0400

    Add logout button if user logged in and functionality to logout

--- client/src/Conponents/Nav/index.js
+++ client/src/Conponents/Nav/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import { Link, useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import { Button } from '@material-ui/core';
 import "./style.css";
+import { logout } from '../../Actions/userActions';
 
 const Nav = () => {
+    const dispatch = useDispatch();
+    // Get login Info from state
+    const userLoginInfo = useSelector(state => state.userLogin);
+    const { userInfo } = userLoginInfo;
+
+    // Determines browser location
     const locationVar = useLocation();
     let location = "home";
 
     if (locationVar.pathname === "/login") location = "login";
     else if (locationVar.pathname === "/register") location = "register";
+
+    // Logs user out when logout button clicked
+    const handleLogout = (e) => {
+        e.preventDefault();
+        dispatch(logout());
+    };
 
     return (
         <nav className={`nav nav-${location}`}>
@@ -17,7 +32,14 @@ const Nav = () => {
             <div className="nav-right">
                 {/* Only show login if not on login page */}
                 {!(locationVar.pathname === "/login" || locationVar.pathname === "/register") ? (
-                    <span><Link to="/login">Login</Link></span>
+                    !userInfo ? (
+                        <span><Link to="/login">Login</Link></span>
+                    ) : (
+                        <span><Button
+                            onClick={e => handleLogout(e)}
+                            variant="text" size="large" className={"text-light btn-large"}
+                        >Logout</Button></span>
+                    )
                 ) : <div></div>}
             </div>
         </nav>
