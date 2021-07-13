713c46cfa1b4ed9481099e71067f4aa6fed8f15c
Nathan Peek <nmp14fsu@gmail.com>
2021-07-13 02:27:06 -0400

    Add user constants, actions, and reducers top redux store for logging in and out

--- client/src/store.js
+++ client/src/store.js
@@ -1,14 +1,15 @@
 import { createStore, compose, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
+import { userLoginReducer } from "./Reducers/userReducers";
 
 const initialState = {
     userLogin: {
-        userInfo: localStorage.getItem("userInfo") ? JSON.parse(localStorage.getItem("userInfo")) : null
+        userInfo: localStorage.getItem("userInfo") ? JSON.parse(localStorage.getItem("userInfoMoods")) : null
     }
 }
 
 const reducer = combineReducers({
-
+    userLogin: userLoginReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
