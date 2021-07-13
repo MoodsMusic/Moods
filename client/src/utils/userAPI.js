b5fb2023aa517134177d02aa565702ddb5869579
Nathan Peek <nmp14fsu@gmail.com>
2021-07-13 03:06:37 -0400

    Create API fetch for user register

--- client/src/utils/userAPI.js
+++ client/src/utils/userAPI.js
@@ -6,3 +6,13 @@ export const userLogin = async (email, password) => {
         password: password
     });
 };
+
+export const userRegister = async (first_name, last_name, username, email, password) => {
+    return await axios.post("/api/users/register", {
+        first_name,
+        last_name,
+        username,
+        email,
+        password
+    });
+};
