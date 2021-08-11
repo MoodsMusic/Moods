012da045b9d5607f2ede8f46c105346632dc2b32
Nathan Peek <nmp14fsu@gmail.com>
2021-08-11 18:25:48 -0400

    Update import

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -11,7 +11,7 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import MessageBox from "../Conponents/MessageBox";
+import MessageBox from "../Components/MessageBox";
 import { registerAction } from '../Actions/userActions';
 
 const useStyles = makeStyles((theme) => ({
